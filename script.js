169e675cd42eb0b6f727c14720ee724653d23e8e
Tiago Borges <103573703+TFellix19@users.noreply.github.com>
2023-01-05 20:16:18 +0000

    little changes on script

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
   const image = document.querySelector("#profile img")
   const txt = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    image.setAttribute("src", "./assets/avatar-light.png")
+    image.setAttribute("src", "./assets/Avatar-light.png")
     txt.setAttribute("alt", "foto eu editada")
   } else {
-    image.setAttribute("src", "./assets/avatar.png")
+    image.setAttribute("src", "./assets/Avatar.png")
     txt.setAttribute("alt", "foto eu original")
   }
 }
